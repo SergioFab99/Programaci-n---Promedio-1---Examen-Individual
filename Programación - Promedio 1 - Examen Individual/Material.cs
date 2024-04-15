cc9b8a914a2f6af9e34ce088d4c4e07d7b8d35fc
Sergio Fabián <2021158559@talento.tls.edu.pe>
2024-04-15 17:37:12 -0500

    Clase material
    se añadió la clase material

---
+++ Programación - Promedio 1 - Examen Individual/Material.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programación___Promedio_1___Examen_Individual
+{
+    class Material
+    {
+        protected string nombre;
+
+        public Material(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public string getNombre()
+        {
+            return nombre;
+        }
+    }
+
+}
